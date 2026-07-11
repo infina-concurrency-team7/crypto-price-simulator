d96e611214ee1c7a6c9c95a807e9d3ae9c8f5303
miray <tepemiray95@gmail.com>
2026-07-11 11:36:21 +0300

    Initial project setup

---
+++ src/main/java/com/infina/cryptopricesimulator/CryptoPriceSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.infina.cryptopricesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPriceSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPriceSimulatorApplication.class, args);
+	}
+
+}
